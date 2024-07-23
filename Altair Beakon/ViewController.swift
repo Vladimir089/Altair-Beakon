b9104a16905b181aaca964831167495cf158fe42
Vladimir089 <pickerpikcer@gmail.com>
2024-07-23 16:35:12 +0300

    Initial Commit

---
+++ Altair Beakon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Altair Beakon
+//
+//  Created by Владимир Кацап on 23.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
